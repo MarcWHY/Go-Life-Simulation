15f34289827d30447a68504b33272e2e817bd911
Marc W <59051233+MarcWHY@users.noreply.github.com>
2023-07-20 13:35:08 +0800

    Update main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+using namespace std;
 
+class Player
+{
+public:
+  Player(string name, int health, int elo, int wealth) {
+    this->name = name;
+    this->health = health;
+    this->strength = elo;
+    this->agility = wealth;
+  }
+private:
+  string name;
+  int health;
+  int elo;
+  int wealth;
+};
+
+int main()
+{
+  return 0;
+}
